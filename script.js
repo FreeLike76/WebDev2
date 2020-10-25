fc2ccede72c4449a7fdd725dc9410a2b4305c81c
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-10-25 14:04:11 +0200

    added animation

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let upButton = document.querySelector(".upButton");
 
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky", window.scrollY > 50);
+    upButton.classList.toggle("anim",window.scrollY > 0);
 })
