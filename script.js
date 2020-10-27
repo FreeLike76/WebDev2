d92aa30a948b6953493333b981ae374e1a73cd10
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-10-27 11:50:59 +0200

    Almost done

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let header = document.querySelector("header");
 let upButton = document.querySelector(".upButton");
+let formCheck = document.getElementById("formRules");
+let formButton = document.getElementById("formButton");
+
+formCheck.onchange = function() {
+    formButton.disabled = !this.checked;
+  };
 
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky", window.scrollY > 50);
